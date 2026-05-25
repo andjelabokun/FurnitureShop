1c71f031b0de79bf45d42ccc46a83d022708cf96
andjelabokun <andjelabokun@gmail.com>
2026-05-25 20:14:39 +0200

    Povezivanje sa frontendom

--- SalonNamestaja/Program.cs
+++ SalonNamestaja/Program.cs
@@ -7,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -19,6 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowReact");
 app.UseAuthorization();
 app.MapControllers();
 
